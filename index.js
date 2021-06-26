40675f39e326efc8be07ad86711c0d0a18ba0f2d
Alan <40917647+alanlichen@users.noreply.github.com>
2021-06-26 21:28:21 +0000

    Ace & deps

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const mongoose = require('mongoose');
 const app = express();
 
 // Using ejs
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -20,92 +20,67 @@ app.use('/static', express.static('public'));
 app.use(express.json());
 
 mongoose.connect(process.env.URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useFindAndModify: false,
 });
 
 const genRandomString = (len) => {
-    const str = 'ABCDEFGHIJKLMNOPQRSTUVWDYZabcdefghijklmnopqrstuvwdyz1234567890';
-    let hex = '';
-    for (i = 0; i < len; i++) {
-        hex += str.charAt(Math.floor(Math.random() * str.length));
-    }
-    return hex;
-}
+	const str =
+		'ABCDEFGHIJKLMNOPQRSTUVWDYZabcdefghijklmnopqrstuvwdyz1234567890';
+	let hex = '';
+	for (i = 0; i < len; i++) {
+		hex += str.charAt(Math.floor(Math.random() * str.length));
+	}
+	return hex;
+};
 
-
-app.get('/', async (req, res) => {
-    const id = req.query.id;
-    if (id) {
-        const data = await BinSchema.findOne({ id })
-        if (!data) {
-            return res.render('pages/error', { error: `Could not find paste with id: ${id} in the database!` })
-        }
-        res.render('pages/index', { text: data.text });
-    } else {
-        res.redirect('/make')
-    }
+app.get('/', (req, res) => {
+	return res.redirect('/make');
 });
 
 app.get('/make', (req, res) => {
-    res.render('pages/make');
+	const text = req.query.text || 'Text here';
+	return res.render('pages/make', { text });
 });
 
 app.post('/new', async (req, res) => {
-    const text = req.body.text;
+	const text = req.body.text;
+	const lang = req.body.lang;
 
-    let id = genRandomString(6);
+	if (!text) return res.send('no u');
 
-    // This is just so we dont over write something thats already in the database
-    const check = await BinSchema.findOne({ id })
-    if (check) {
-        id = genRandomString(6)
-    }
+	let id = genRandomString(6);
 
-    await BinSchema.findOneAndUpdate(
-        {
-            id
-        },
-        {
-            id,
-            text,
-        },
-        {
-            upsert: true
-        }
-    )
-    res.redirect(`/?id=${id}`);
+	// This is just so we dont over write something thats already in the database
+	const check = await BinSchema.findOne({ id });
+	if (check) {
+		id = genRandomString(6);
+	}
+
+	await new BinSchema({
+		id,
+		text,
+		lang,
+	}).save();
+	res.json({ id });
 });
 
-app.post('/api/new', async (req, res) => {
-    const text = req.body.text;
-
-    let id = genRandomString(6);
-
-    const check = await BinSchema.findOne({ id })
-    if (check) {
-        id = genRandomString(6)
-    }
-
-    await BinSchema.findOneAndUpdate(
-        {
-            id
-        },
-        {
-            id,
-            text,
-        },
-        {
-            upsert: true
-        }
-    )
-    res.json({
-        id: id,
-        url: `https://bin.alanchen12.repl.co?id=${id}`
-    });
-})
+app.get('/:id', async (req, res) => {
+	const id = req.params.id;
+	if (id) {
+		const data = await BinSchema.findOne({ id });
+		if (!data) {
+			return res.render('pages/error', {
+				error: `Could not find paste with id: ${id} in the database!`,
+			});
+		}
+		res.render('pages/index', { text: data.text, lang: data.lang, id });
+	} else {
+		res.redirect('/make');
+	}
+});
 
 app.listen(process.env.SERVER_PORT, () => {
-    console.log(`Server up on port ${process.env.SERVER_PORT}`);
+	console.log(`Server up on port ${process.env.SERVER_PORT}`);
 });
