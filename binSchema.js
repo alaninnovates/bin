a73646ad9a9f7797fe6b107814af7abdf2d0962c
Alan <40917647+alanlichen@users.noreply.github.com>
2021-06-26 21:38:55 +0000

    update schema and simplify things

--- binSchema.js
+++ binSchema.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const BinSchema = mongoose.Schema({
     id: String,
     text: String,
+	lang: String,
 });
 
 module.exports = mongoose.model('bin', BinSchema);
